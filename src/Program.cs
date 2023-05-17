9b5d212fb5cfca3b1636a38d2f4024735409fced
Rino Reji Cheriyan <mailrino@gmail.com>
2023-05-17 20:14:19 +0000

    added spa support to minial api proj

--- src/Program.cs
+++ src/Program.cs
@@ -1,6 +1,20 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/api", () => "Hello World!");
+app.MapGet("/api/{name}", (string name) => $"Hello {name}!");
 
+app.UseRouting();
+app.UseEndpoints(_ => {});
+app.Use((ctx,next)=>{
+    if(ctx.Request.Path.StartsWithSegments("/api")){
+        ctx.Response.StatusCode = 404;
+        return Task.CompletedTask;
+    }
+    return next();
+});
+
+app.UseSpa(x=>{
+    x.UseProxyToSpaDevelopmentServer("https://3000-rinoreji-digitaldiary-6owqkgb9h75.ws-us97.gitpod.io");
+});
 app.Run();
